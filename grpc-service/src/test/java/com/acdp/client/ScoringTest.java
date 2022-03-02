479cc4c4e787c00eff29c5fbbb227b36a804f31f
santoshkundlik <santosh.kundlik@gmail.com>
2022-03-01 23:55:27 -0500

    Updated Junit Test

--- grpc-service/src/test/java/com/acdp/client/ScoringTest.java
+++ grpc-service/src/test/java/com/acdp/client/ScoringTest.java
@@ -2,21 +2,33 @@ package com.acdp.client;
 
 import com.acdp.grpc.scoreGrpc;
 import com.acdp.grpc.Score.ScoreRequest;
+import com.acdp.service.ScoreService;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
 
 public class ScoringTest {
 
     private ManagedChannel channel;
     private scoreGrpc.scoreStub clientStub;
 
+    /*The server runs on the port 9099*/
+    Server server = ServerBuilder.forPort(9099).addService(new ScoreService()).build();
+
     @Before
-    public void setup(){
-        this.channel = ManagedChannelBuilder.forAddress("localhost", 9091)
+    public void setup() throws IOException, InterruptedException {
+
+        server.start();
+        Thread.sleep(1000);
+
+        this.channel = ManagedChannelBuilder.forAddress("localhost", 9099)
                 .usePlaintext()
                 .build();
         this.clientStub = scoreGrpc.newStub(channel);
@@ -30,6 +42,7 @@ public class ScoringTest {
 
         // just for testing
         Thread.sleep(10000);
+        server.shutdown();
     }
 
     @After
